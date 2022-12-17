d9766b62b9133f61f44ef09891f1e08ac839c933
WildBanjos <zach012ary@gmail.com>
2022-12-17 03:14:27 -0600

    ReviewBlacklist works. LoadConfig works, keys are imported as lowercase. Logging works. Still need to get final BlacklistTripped block working.

--- scripts/DiffusionDefender.py
+++ scripts/DiffusionDefender.py
@@ -10,7 +10,7 @@ from modules import processing, shared, images
 from modules.processing import Processed, process_images, images
 from modules.shared import opts, cmd_opts, state
 from pathlib import Path
-
+from distutils.util import strtobool
 
 pth = scripts.basedir()
 logfile = os.path.join(pth,'Defender.log')
@@ -27,16 +27,19 @@ log.addHandler(file_handler)
 def FindAndReplace(StringToReview,Replacements):
     TotalReplacements = 0
     for key, value in Replacements.items():
-        (StringToReview, count) = re.subn(key, value, StringToReview)
+        (ReviewedString, count) = re.subn(key, value, StringToReview.lower())
         TotalReplacements = TotalReplacements + int(count)
+    if ReviewedString.lower() == StringtoReview.lower():
+        ReviewedString = StringToReview
     return StringToReview, TotalReplacements
 
 
 def ReviewBlacklist(StringToReview,blacklist): #This works
+    result = False
     for words in blacklist:
-        if re.search(words,StringToReview) != None:
-            return True
-    return False
+        if re.search(words,StringToReview.lower()) != None:
+            result = True
+    return result
 
 def LoadConfig(): #How do I test if this works? Logging.
     #Load Options
@@ -49,9 +52,13 @@ def LoadConfig(): #How do I test if this works? Logging.
     config_options = {}
     config = configparser.ConfigParser()
     config.read(configfile)
-    select = {"True","False"}
+
     config_options = dict(config.items('DEFAULT'))
-    config_options['UseBlacklist'] #stopped here
+    log.info(f'Config read as: {json.dumps(config_options)}')
+    booloptions = ['useblacklist', 'usefindandreplace','addtolog']
+    for option in booloptions:
+        config_options[option] = bool(strtobool(config_options[option]))
+
 
     #UseBlacklist = config['Default'].getboolean('UseBlacklist')
     #UseFindAndReplace = config['Default'].getboolean('UseFindAndReplace')
@@ -64,14 +71,16 @@ def LoadConfig(): #How do I test if this works? Logging.
 
     #Load blacklist
     blacklist = []
-    with open(blacklistfile,'r') as f: blacklist = f.read().splitlines()
-    log.info(f'Loaded {len(blacklist)} items from blacklist')
+    if config_options['useblacklist']:
+        with open(blacklistfile,'r') as f: blacklist = f.read().splitlines()
+        log.info(f'Loaded {len(blacklist)} items from blacklist')
 
     #Load Replacement pairs
     replace_dict = {}
-    config.read(replacementfile)
-    replace_dict = dict(config.items('Replacements'))
-    log.info(f'Loaded {len(replace_dict)} replacements')
+    if config_options['usefindandreplace']:
+        config.read(replacementfile)
+        replace_dict = dict(config.items('DEFAULT'))
+        log.info(f'Loaded {len(replace_dict)} replacement pairs')
 
     return replace_dict, blacklist, config_options #UseBlacklist, UseFindAndReplace, BlacklistBehavior,AddToLog,CustomPrompt
 
@@ -97,15 +106,16 @@ class Script(scripts.Script):
         BlacklistTripped = False
 
         prompt = p.prompt
+        new_prompt = ""
 
         try:
-            if config_options['UseBlacklist']:
+            if config_options['useblacklist']:
                 BlacklistTripped = ReviewBlacklist(prompt,blacklist)
-        except Exception:
-            log.error('Unable to reference prompt against blacklist')
+        except Exception as ex:
+            log.error(f'Unable to reference prompt against blacklist: {type(ex)}')
 
         try:
-            if config_options['UseFindAndReplace']:
+            if config_options['usefindandreplace']:
                 new_prompt,TotalReplacements = FindAndReplace(prompt,replacements)
                 p.prompt = new_prompt
         except Exception:
@@ -113,41 +123,45 @@ class Script(scripts.Script):
 
         #Logging
         try:
-            if config['AddToLog'] and BlacklistTripped or TotalReplacements > 0:
+            if config_options['addtolog']:
                 if BlacklistTripped:
-                        log.info(f'BLACKLIST: {prompt}')
+                    log.info(f'BLACKLIST: {prompt}')
                 if TotalReplacements > 0:
-                        log.info(f'REPLACEMENTS: {prompt} <|> {new_prompt}')
-        except Exception:
-            log.error('Unable to add to log')
+                    log.info(f'REPLACEMENTS: {prompt} <|> {new_prompt}')
+        except Exception as ex:
+            log.error(f'Unable to add to log: {ex=}, {type(ex)}')
+            raise
 
-        BlacklistBehavior = config_options['BlacklistBehavior']
+        BlacklistBehavior = config_options['blacklistbehavior']
         try:
             if BlacklistTripped:
                 if BlacklistBehavior == "StopProcessing":
                     p.batch_size = 1
-                    stop = shared.state.skip()
+                    #shared.state.interru()
+                    log.debug('Ending Prompt')
+                    shared.state.end()
+                    return
                 elif BlacklistBehavior == "ReturnBlank":
                     prompt = ""
                     p.batch_size = 1
-                    stop = shared.state.skip()
+                    p.prompt = prompt
+                    log.debug('Replaced with blank prompt of empty string')
+                    return
                 elif BlacklistBehavior == "ReturnPrompt":
-                    p.prompt = CustomPrompt
+                    p.prompt = config_options['customprompt']
                     p.batch_size = 1
-                    processed = process_images(p)
-                    return processed
+                    log.debug(f'Returned custom prompt of {p.prompt}')
                 elif BlacklistBehavior == "LogOnly":
-                    if not AddToLog:
+                    if not config_options['addtolog']:
                         log.info(f'BLACKLIST: {prompt}')
-                    processed = process_images(p)
-                    return processed
                     #continue and send prompt
                 elif BlacklistBehavior == "NoAction":
-                    processed = process_images(p)
-                    return processed
+                    log.debug('No Action Taken')
                 else:
-                    logging.warning("ScriptName: BlacklistBehavior is not set in .ini file, defaulting to NoAction")
-                    processed = process_images(p)
-                    return processed
-        except Exception:
-            log.error('Unable to Enforce Blacklist Behavior')
+                    log.warning("ScriptName: BlacklistBehavior is not set in .ini file, defaulting to NoAction")
+                #proc = process_images(p) Doesn't look like I need this?
+                return p
+        except Exception as ex:
+            log.error(f'Unable to Enforce Blacklist Behavior: {ex=}, {type(ex)}')
+            raise
+            return
